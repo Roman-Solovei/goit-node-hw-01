5cfb80cabab096341269a5f3e2151e34066045f9
Roman-Solovei <romario2409@gmail.com>
2022-05-23 02:59:10 +0300

    01-03

--- contacts.js
+++ contacts.js
@@ -1,18 +1,80 @@
 const fs = require('fs').promises;
 const path = require('path');
+const uuid = require('uuid');
 
-const contactsPath = path.resolve('./db/contacts.json');
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 
-async function listContacts() {
- try {
-     const data = await fs.readFile(contactsPath, 'utf8');
-     console.table(data);
-     return JSON.parse(data);
- } catch (error) {console.log(error);
-     
- }
+
+
+async function listContacts(){
+    try {
+        const contacts = await getDataContacts();
+        console.table(contacts);
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 
-module.exports = { listContacts }
+async function getDataContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await getDataContacts();
+    const neededContact = await contacts.filter((contact) => {
+      return contact.id === contactId;
+    });
+    console.table(neededContact);
+}
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: uuid.v4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  try {
+    const allContacts = await getDataContacts();
+    allContacts.push(newContact);  
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts), "utf8");
+    console.table(allContacts)
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await getDataContacts();
+    const delContacts = await contacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    await fs.writeFile(contactsPath, JSON.stringify(delContacts), "utf8");
+    console.table(delContacts);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-const { listContacts } = require('./contacts');
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.log('Hello  det')
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
+
+// invokeAction({action:'removeContact', id:'6'});
+
+// getContactById('5').then(value => console.log(value));
+// listContacts().then(value => console.log(value));
+
+
+
+
+
+
+
+// const path = require('path');
+// const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+// console.log(contactsPath)
+
+//  addContact('asd', 'emai', '1232434')
+// .then(value => console.log(value))
+// listContacts()
+// getContactById('1').then(value => console.log(value))
+// removeContact('3')
